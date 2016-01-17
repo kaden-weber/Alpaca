a2f04fb6d5cd6374c18a25fc9c135b92171e6b8d
Leandro Pereira de Lima e Silva <leandro@limaesilva.com.br>
2016-01-17 13:15:57 -0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(name='mulandweb',
+      version='0.1',
+      description='Web Interface for the Muland Application',
+      url='https://bitbucket.org/leandropls/mulandweb',
+      author='Leandro Pereira de Lima e Silva',
+      author_email='leandro@limaesilva.com.br',
+      license='MIT',
+      packages=['mulandweb'],
+      install_requires=[
+        'bottle',
+        'gunicorn',
+      ]
+      zip_safe=False)
