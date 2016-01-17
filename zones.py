f2ce5c45138673c26a6e22d535fc92338f9eb611
Leandro Pereira de Lima e Silva <leandro@limaesilva.com.br>
2016-01-17 03:01:23 -0200

    first draft of the zones module

---
+++ zones.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Text
+from geoalchemy2 import Geometry
+
+engine = create_engine('postgresql://gis:gis@localhost/gis')
+Base = declarative_base()
+
+class Zones(Base):
+    __tablename__ = 'zones'
+    id = Column(Integer, primary_key=True)
+    area = Column(Geometry('POLYGON', srid=900913, spatial_index=True))
+    info = Column(Text)
