18433496f5d3e02dd64afc03453cf07ccd143e6e
Leandro Pereira de Lima e Silva <leandro@limaesilva.com.br>
2016-01-23 19:56:43 -0200

    fixed spec errors on db

--- mulandweb/db.py
+++ mulandweb/db.py
@@ -2,6 +2,7 @@
 
 from sqlalchemy import create_engine, Table, Column, MetaData
 from sqlalchemy import Integer, String, Float, ForeignKey, Sequence
+from sqlalchemy import ForeignKeyConstraint
 from sqlalchemy.dialects.postgresql import ARRAY
 from geoalchemy2 import Geometry
 from . import config
@@ -29,25 +30,28 @@ markets = Table('markets', meta,
 
 rent_adjustments = Table('rent_adjustments', meta,
     Column('types_id', Integer, ForeignKey('real_estate_types.id'), primary_key=True),
-    Column('zones_id', Integer, ForeignKey('zones.id'), primary_key=True),
+    Column('zones_id', Integer, primary_key=True),
     Column('models_id', Integer, ForeignKey('models.id'), primary_key=True),
     Column('adjustment', Float),
+    ForeignKeyConstraint(['zones_id', 'models_id'], ['zones.id', 'zones.models_id']),
 )
 
 supply = Table('supply', meta,
     Column('types_id', Integer, ForeignKey('real_estate_types.id'), primary_key=True),
-    Column('zones_id', Integer, ForeignKey('zones.id'), primary_key=True),
+    Column('zones_id', Integer, primary_key=True),
     Column('models_id', Integer, ForeignKey('models.id'), primary_key=True),
     Column('nrest', Float),
+    ForeignKeyConstraint(['zones_id', 'models_id'], ['zones.id', 'zones.models_id']),
 )
 
 real_estates_zones = Table('real_estates_zones', meta,
     Column('types_id', Integer, ForeignKey('real_estate_types.id'), primary_key=True),
-    Column('zones_id', Integer, ForeignKey('zones.id'), primary_key=True),
+    Column('zones_id', Integer, primary_key=True),
     Column('markets_id', Integer, ForeignKey('markets.id'), primary_key=True),
     Column('models_id', Integer, ForeignKey('models.id'), primary_key=True),
     Column('header', ARRAY(String, zero_indexes=True)),
     Column('data', ARRAY(Float, zero_indexes=True)),
+    ForeignKeyConstraint(['zones_id', 'models_id'], ['zones.id', 'zones.models_id']),
 )
 
 agents = Table('agents', meta,
@@ -55,7 +59,7 @@ agents = Table('agents', meta,
     Column('models_id', Integer, ForeignKey('models.id'), primary_key=True),
     Column('markets_id', Integer, ForeignKey('markets.id')),
     Column('aggra_id', Integer),
-    Column('upperbb', Float)
+    Column('upperbb', Float),
     Column('data', ARRAY(Float, zero_indexes=True)),
 )
 
@@ -67,34 +71,41 @@ zones = Table('zones', meta,
 )
 
 demand = Table('demand', meta,
-    Column('agents_id', Integer, ForeignKey('agents.id'), primary_key=True),
+    Column('agents_id', Integer, primary_key=True),
     Column('models_id', Integer, ForeignKey('models.id'), primary_key=True),
     Column('demand', Float),
+    ForeignKeyConstraint(['agents_id', 'models_id'], ['agents.id', 'agents.models_id']),
 )
 
 subsidies = Table('subsidies', meta,
-    Column('agents_id', Integer, ForeignKey('agents.id'), primary_key=True),
+    Column('agents_id', Integer, primary_key=True),
     Column('types_id', Integer, ForeignKey('real_estate_types.id'), primary_key=True),
-    Column('zones_id', Integer, ForeignKey('zones.id'), primary_key=True),
+    Column('zones_id', Integer, primary_key=True),
     Column('models_id', Integer, ForeignKey('models.id'), primary_key=True),
     Column('subsidies', Float),
+    ForeignKeyConstraint(['zones_id', 'models_id'], ['zones.id', 'zones.models_id']),
+    ForeignKeyConstraint(['agents_id', 'models_id'], ['agents.id', 'agents.models_id']),
 )
 
 demand_exogenous_cutoff = Table('demand_exogenous_cutoff', meta,
-    Column('agents_id', Integer, ForeignKey('agents.id'), primary_key=True),
+    Column('agents_id', Integer, primary_key=True),
     Column('types_id', Integer, ForeignKey('real_estate_types.id'), primary_key=True),
-    Column('zones_id', Integer, ForeignKey('zones.id'), primary_key=True),
+    Column('zones_id', Integer, primary_key=True),
     Column('models_id', Integer, ForeignKey('models.id'), primary_key=True),
     Column('dcutoff', Float),
+    ForeignKeyConstraint(['zones_id', 'models_id'], ['zones.id', 'zones.models_id']),
+    ForeignKeyConstraint(['agents_id', 'models_id'], ['agents.id', 'agents.models_id']),
 )
 
 agents_zones = Table('agents_zones', meta,
-    Column('agents_id', Integer, ForeignKey('agents.id'), primary_key=True),
-    Column('zones_id', Integer, ForeignKey('zones.id'), primary_key=True),
+    Column('agents_id', Integer, primary_key=True),
+    Column('zones_id', Integer, primary_key=True),
     Column('models_id', Integer, ForeignKey('models.id'), primary_key=True),
     Column('acc', Float),
     Column('att', Float),
     Column('data', ARRAY(Float, zero_indexes=True)),
+    ForeignKeyConstraint(['zones_id', 'models_id'], ['zones.id', 'zones.models_id']),
+    ForeignKeyConstraint(['agents_id', 'models_id'], ['agents.id', 'agents.models_id']),
 )
 
 #meta.create_all(engine)
