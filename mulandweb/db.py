3fb1bd6d59fb7c03a75fca15a3827514a33e9932
Leandro Pereira de Lima e Silva <leandro@limaesilva.com.br>
2016-01-23 15:11:32 -0200

    added models table

--- mulandweb/db.py
+++ mulandweb/db.py
@@ -8,6 +8,11 @@ import config
 engine = create_engine(config.db_url)
 meta = MetaData()
 
+models = Table('models', meta,
+    Column('id', Integer, Sequence('models_id_seq'), primary_key=True),
+    Column('name', String),
+)
+
 real_estate_types = Table('real_estate_types', meta,
     Column('id', Integer, primary_key=True),
     Column('description', String),
@@ -21,12 +26,14 @@ markets = Table('markets', meta,
 rent_adjustments = Table('rent_adjustments', meta,
     Column('types_id', None, ForeignKey('real_estate_types.id'), primary_key=True),
     Column('zones_id', None, ForeignKey('zones.id'), primary_key=True),
+    Column('models_id', None, ForeignKey('models.id'), primary_key=True),
     Column('adjustment', Float),
 )
 
 supply = Table('supply', meta,
     Column('types_id', None, ForeignKey('real_estate_types.id'), primary_key=True),
     Column('zones_id', None, ForeignKey('zones.id'), primary_key=True),
+    Column('models_id', None, ForeignKey('models.id'), primary_key=True),
     Column('nrest', Float),
 )
 
@@ -34,12 +41,14 @@ real_estates_zones = Table('real_estates_zones', meta,
     Column('types_id', None, ForeignKey('real_estate_types.id'), primary_key=True),
     Column('zones_id', None, ForeignKey('zones.id'), primary_key=True),
     Column('markets_id', None, ForeignKey('markets.id'), primary_key=True),
+    Column('models_id', None, ForeignKey('models.id'), primary_key=True),
     Column('header', ARRAY(String, zero_indexes=True)),
     Column('data', ARRAY(Float, zero_indexes=True)),
 )
 
 agents = Table('agents', meta,
     Column('id', Integer, primary_key=True),
+    Column('models_id', None, ForeignKey('models.id'), primary_key=True),
     Column('markets_id', None, ForeignKey('markets.id')),
     Column('aggra_id', Integer),
     Column('header', ARRAY(String, zero_indexes=True)),
@@ -48,6 +57,7 @@ agents = Table('agents', meta,
 
 zones = Table('zones', meta,
     Column('id', Integer, primary_key=True),
+    Column('models_id', None, ForeignKey('models.id'), primary_key=True),
     Column('area', Geometry('POLYGON', srid=900913, spatial_index=True)),
     Column('header', ARRAY(String, zero_indexes=True)),
     Column('data', ARRAY(Float, zero_indexes=True)),
@@ -55,6 +65,7 @@ zones = Table('zones', meta,
 
 demand = Table('demand', meta,
     Column('id', Integer, primary_key=True),
+    Column('models_id', None, ForeignKey('models.id'), primary_key=True),
     Column('demand', Float),
 )
 
@@ -62,6 +73,7 @@ subsidies = Table('subsidies', meta,
     Column('demand_id', None, ForeignKey('demand.id'), primary_key=True),
     Column('types_id', None, ForeignKey('real_estate_types.id'), primary_key=True),
     Column('zones_id', None, ForeignKey('zones.id'), primary_key=True),
+    Column('models_id', None, ForeignKey('models.id'), primary_key=True),
     Column('subsidies', Float),
 )
 
@@ -69,12 +81,14 @@ demand_exogenous_cutoff = Table('demand_exogenous_cutoff', meta,
     Column('demand_id', None, ForeignKey('demand.id'), primary_key=True),
     Column('types_id', None, ForeignKey('real_estate_types.id'), primary_key=True),
     Column('zones_id', None, ForeignKey('zones.id'), primary_key=True),
+    Column('models_id', None, ForeignKey('models.id'), primary_key=True),
     Column('dcutoff', Float),
 )
 
 agents_zones = Table('agents_zones', meta,
     Column('demand_id', None, ForeignKey('demand.id'), primary_key=True),
     Column('zones_id', None, ForeignKey('zones.id'), primary_key=True),
+    Column('models_id', None, ForeignKey('models.id'), primary_key=True),
     Column('acc', Float),
     Column('att', Float),
     Column('header', ARRAY(String, zero_indexes=True)),
